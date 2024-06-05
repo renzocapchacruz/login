3b0758c143523d9d050248a7a4efbbcf00812cba
renzocapchacruz <160557613+renzocapchacruz@users.noreply.github.com>
2024-06-04 22:08:45 -0500

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const links = document.querySelectorAll(".link");
 const forms = document.querySelector(".forms");
 const showHide = document.querySelectorAll(".hide-show");
-const passwordFields = document.querySelectorAll(".field input[type='password']");
 
 showHide.forEach(showicon=>{
     showicon.addEventListener("click", ()=>{
-        const passwordField = showicon.parentNode.querySelector("input[type='password']");
-        if(passwordField.type === 'password'){
-            passwordField.type = 'text';
-            showicon.classList.replace("bx-hide", "bx-show");
-        } else {
-            passwordField.type = 'password';
-            showicon.classList.replace("bx-show", "bx-hide");
-        }
+        let field_password = showicon.parentElement.parentElement.querySelectorAll(".password");
+
+        field_password.forEach(password=>{
+            if(password.type === 'password'){
+                password.type = 'text';
+                showHide.classList.replace("bx-hide", "bx-show");
+            }
+            password.type = "password";
+            showHide.classList.replace("bx-show", "bx-hide");
+        })
     })
 })
+
 
 links.forEach(link =>{
     link.addEventListener("click", e=>{
         e.preventDefault();
         forms.classList.toggle("Show-Signup");
-        const currentForm = link.parentNode.querySelector(".form");
-        const otherForm = currentForm.classList.contains("login") ? forms.querySelector(".form.signup") : forms.querySelector(".form.login");
-        currentForm.classList.toggle("hidden");
-        otherForm.classList.toggle("hidden");
     })
 })
